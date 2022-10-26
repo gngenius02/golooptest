d6f030832359a963ba104a1f90882084e019f85d
Nino <gngenius02@gmail.com>
2022-10-25 18:17:17 -0700

    update

--- main.go
+++ main.go
@@ -12,12 +12,11 @@ type HS struct {
 	HashList HashArray
 }
 
-func hashit(s string) string {
-	digest := sha256.Sum256([]byte(s))
-	return hex.EncodeToString(digest[:])
-}
-
 func (h *HS) GetHashUsingArray() {
+	hashit := func(s string) string {
+		digest := sha256.Sum256([]byte(s))
+		return hex.EncodeToString(digest[:])
+	}
 	hl := h.HashList
 	for i := 1; i < len(hl); i++ {
 		(hl)[i] = hashit((hl)[i-1])
@@ -26,9 +25,13 @@ func (h *HS) GetHashUsingArray() {
 }
 
 func GetHashUsingLoop(s string, loops int) string {
-	hash := hashit(s)
-	for i := 0; i < loops-1; i++ {
-		hash = hashit(hash)
+	hashit := func(s *string) {
+		digest := sha256.Sum256([]byte(*s))
+		*s = hex.EncodeToString(digest[:])
+	}
+	hash := s
+	for i := 0; i < loops; i++ {
+		hashit(&hash)
 	}
 	return hash
 }
