25c3e3bfd80ddeeb410b7064f168d2eb3518c253
Nino <gngenius02@gmail.com>
2022-10-26 16:53:10 -0700

    trying to optimize the hashing loop routine.

--- main.go
+++ main.go
@@ -36,71 +36,12 @@ func (HS *HASHSTRUCT) HashLoop(loops int) {
 	}
 }
 
-// func (h *HashThis) HashLoop(loops int) {
-// 	var buf bytes.Buffer
-
-// 	buf.WriteString(h.inputStr)
-// 	// hash.Write(buf.Bytes())
-// 	hash.Sum(buf.Bytes())
-// 	// h.digest = sha256.Sum256(buf.Bytes())
-// 	buf.Reset()
-// 	// h.hashStr = hex.EncodeToString(h.digest[:])
-// 	// for i := 0; i < loops-1; i++ {
-// 	// 	h.digest = sha256.Sum256([]byte(h.hashStr))
-// 	// 	h.hashStr = hex.EncodeToString(h.digest[:])
-// 	// }
-// }
-
 func main() {
 
 	loops := 2
 
 	HS := &HASHSTRUCT{s: "abc"}
-	// HS.s = "a65e2e1666763cceac4458e8bd26e0134dfa09178c8b106521265fe279c2e85c"
 	HS.HashLoop(loops)
 
-	// HashLoop(result, loops)
-
 	fmt.Printf("%x", HS.b)
-
-	// hash := sha256.New()
-
-	// buf.WriteString("abc")
-	// hash.Write(buf.Bytes())
-	// next := hex.EncodeToString(hash.Sum(nil))
-	// buf.Reset()
-	// hash.Reset()
-	// for i := 1; i < loops; i++ {
-	// 	buf.WriteString(next)
-	// 	hash.Write(buf.Bytes())
-	// 	next = hex.EncodeToString(hash.Sum(nil))
-	// 	buf.Reset()
-	// 	hash.Reset()
-	// }
-	// fmt.Printf("%s\n", next)
-
-	// argv := os.Args
-	// argc := len(argv)
-	// command := argv[0]
-	// if argc < 2 {
-	// 	fmt.Printf("Usage:\n\t command <string> [number of loops Default = 100Million]\nExample:\n\t%s abc\n\n\t%s abc 100\n", command, command)
-	// 	os.Exit(1)
-	// }
-	// start := time.Now()
-
-	// input := argv[1]
-	// loops := 100_000_000
-	// if argc > 2 {
-	// 	num, err := strconv.Atoi(argv[2])
-	// 	if err != nil {
-	// 		fmt.Printf("Received 2nd arg but couldnt convert it to int!")
-	// 		os.Exit(1)
-	// 	}
-	// 	loops = num
-	// }
-
-	// hash := HashLoop(input, loops)
-
-	// elapsed := time.Since(start)
-	// fmt.Printf("%s,%s\n\ncompleted in: %s\n", input, hash, elapsed)
 }
