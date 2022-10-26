5905ae50c663e41b99e71fd1940bc1392c71227d
Nino <gngenius02@gmail.com>
2022-10-26 16:52:11 -0700

    trying to optimize the hashing loop routine.

--- main.go
+++ main.go
@@ -2,38 +2,105 @@ package main
 
 import (
 	"crypto/sha256"
-	"encoding/hex"
+	"fmt"
 )
 
-type HashArray []string
-
-type HS struct {
-	LastHash string
-	HashList HashArray
+type HASHSTRUCT struct {
+	s string
+	// buf Buffer
+	b []byte
+	d [32]byte
 }
 
-func (h *HS) GetHashUsingArray() {
-	hashit := func(s string) string {
-		digest := sha256.Sum256([]byte(s))
-		return hex.EncodeToString(digest[:])
+const hextable = "0123456789abcdef"
+
+func (HS *HASHSTRUCT) EncodeToHexBytes() {
+	HS.b = make([]byte, 64)
+	j := 0
+	for _, v := range HS.d {
+		HS.b[j] = hextable[v>>4]
+		HS.b[j+1] = hextable[v&0x0f]
+		j += 2
 	}
-	hl := h.HashList
-	for i := 1; i < len(hl); i++ {
-		(hl)[i] = hashit((hl)[i-1])
-	}
-	h.LastHash = hl[len(hl)-1]
 }
 
-func GetHashUsingLoop(s string, loops int) string {
-	hashit := func(s *string) {
-		digest := sha256.Sum256([]byte(*s))
-		*s = hex.EncodeToString(digest[:])
-	}
-	hash := s
+func (HS *HASHSTRUCT) HashFn() {
+	HS.d = sha256.Sum256(HS.b)
+}
+
+func (HS *HASHSTRUCT) HashLoop(loops int) {
+	HS.b = []byte(HS.s)
 	for i := 0; i < loops; i++ {
-		hashit(&hash)
+		HS.HashFn()
+		HS.EncodeToHexBytes()
 	}
-	return hash
 }
 
-func main() {}
+// func (h *HashThis) HashLoop(loops int) {
+// 	var buf bytes.Buffer
+
+// 	buf.WriteString(h.inputStr)
+// 	// hash.Write(buf.Bytes())
+// 	hash.Sum(buf.Bytes())
+// 	// h.digest = sha256.Sum256(buf.Bytes())
+// 	buf.Reset()
+// 	// h.hashStr = hex.EncodeToString(h.digest[:])
+// 	// for i := 0; i < loops-1; i++ {
+// 	// 	h.digest = sha256.Sum256([]byte(h.hashStr))
+// 	// 	h.hashStr = hex.EncodeToString(h.digest[:])
+// 	// }
+// }
+
+func main() {
+
+	loops := 2
+
+	HS := &HASHSTRUCT{s: "abc"}
+	// HS.s = "a65e2e1666763cceac4458e8bd26e0134dfa09178c8b106521265fe279c2e85c"
+	HS.HashLoop(loops)
+
+	// HashLoop(result, loops)
+
+	fmt.Printf("%x", HS.b)
+
+	// hash := sha256.New()
+
+	// buf.WriteString("abc")
+	// hash.Write(buf.Bytes())
+	// next := hex.EncodeToString(hash.Sum(nil))
+	// buf.Reset()
+	// hash.Reset()
+	// for i := 1; i < loops; i++ {
+	// 	buf.WriteString(next)
+	// 	hash.Write(buf.Bytes())
+	// 	next = hex.EncodeToString(hash.Sum(nil))
+	// 	buf.Reset()
+	// 	hash.Reset()
+	// }
+	// fmt.Printf("%s\n", next)
+
+	// argv := os.Args
+	// argc := len(argv)
+	// command := argv[0]
+	// if argc < 2 {
+	// 	fmt.Printf("Usage:\n\t command <string> [number of loops Default = 100Million]\nExample:\n\t%s abc\n\n\t%s abc 100\n", command, command)
+	// 	os.Exit(1)
+	// }
+	// start := time.Now()
+
+	// input := argv[1]
+	// loops := 100_000_000
+	// if argc > 2 {
+	// 	num, err := strconv.Atoi(argv[2])
+	// 	if err != nil {
+	// 		fmt.Printf("Received 2nd arg but couldnt convert it to int!")
+	// 		os.Exit(1)
+	// 	}
+	// 	loops = num
+	// }
+
+	// hash := HashLoop(input, loops)
+
+	// elapsed := time.Since(start)
+	// fmt.Printf("%s,%s\n\ncompleted in: %s\n", input, hash, elapsed)
+}
--- main_test.go
+++ main_test.go
@@ -4,26 +4,43 @@ import (
 	"testing"
 )
 
-func BenchmarkGetHashUsingArray10Million(b *testing.B) {
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		firstValue := "abc"
-		hs := HS{"", make(HashArray, 10_000_001)}
-		hs.HashList[0] = firstValue
-		hs.GetHashUsingArray()
-		if hs.LastHash != "bf34d93b4be2a313b06cdf9d805c5f3d140abd872c37199701fb1e43fe479923" {
-			b.Error("Unexpected Result: " + hs.LastHash)
-		}
-	}
-}
+// func TestHasherWithEmptyString(t *testing.T) {
+// 	result := HashLoop("", 1)
+// 	if result != "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" {
+// 		t.Fatalf("TestHasherWithEmptyString: FAILED. Unexpected result: %s", result)
+// 	}
+// }
+
+// func TestHasherWith0Loops(t *testing.T) {
+// 	result := HashLoop("abc", 0)
+// 	if result != "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad" {
+// 		t.Fatalf("TestHasherWithEmptyString: FAILED. Unexpected result: %s", result)
+// 	}
+// }
 
 func BenchmarkGetHashUsingLoop10Million(b *testing.B) {
 	b.ReportAllocs()
+
+	HS := &HASHSTRUCT{s: "abc"}
+
 	for i := 0; i < b.N; i++ {
-		firstValue := "abc"
-		result := GetHashUsingLoop(firstValue, 10_000_000)
+		HS.HashLoop(10_000_000)
+		result := string(HS.b)
 		if result != "bf34d93b4be2a313b06cdf9d805c5f3d140abd872c37199701fb1e43fe479923" {
 			b.Error("Unexpected result: " + result)
 		}
 	}
 }
+
+// func BenchmarkGetHashUsingLoop10Million(b *testing.B) {
+// 	b.ReportAllocs()
+// 	var result string
+// 	var firstValue string
+// 	for i := 0; i < b.N; i++ {
+// 		firstValue = "abc"
+// 		result = HashLoop(firstValue, 10_000_000)
+// 		if result != "bf34d93b4be2a313b06cdf9d805c5f3d140abd872c37199701fb1e43fe479923" {
+// 			b.Error("Unexpected result: " + result)
+// 		}
+// 	}
+// }
